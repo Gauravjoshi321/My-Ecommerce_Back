07ed19399c82aee1d2c902797a7f4966a66faf44
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-03-12 18:32:22 +0530

    new model and routes added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
 const { createProduct } = require('./controller/Product');
 const productsRouter = require('./routes/Products');
 const categoriesRouter = require('./routes/Categories');
 const brandsRouter = require('./routes/Brands');
-const cors = require('cors')
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
 
 const server = express();
 
@@ -15,8 +20,12 @@ server.use(express.json()); // to parse req.body
 
 // Routing Division
 server.use('/products', productsRouter.router);
-server.use('/categories', categoriesRouter.router)
-server.use('/brands', brandsRouter.router)
+server.use('/categories', categoriesRouter.router);
+server.use('/brands', brandsRouter.router);
+server.use('/users', usersRouter.router);
+server.use('/auth', authRouter.router);
+server.use('/cart', cartRouter.router);
+server.use('/orders', ordersRouter.router);
 
 
 // Connection to the local DataBase
