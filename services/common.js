12bc93710b771ec3aee9b1c64a065fb1bb7a11cc
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-03-19 19:56:14 +0530

    common.js

--- services/common.js
+++ services/common.js
@@ -4,12 +4,16 @@ exports.isAuth = (req, res, done) => {
   return passport.authenticate('jwt');
 };
 
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
 exports.cookieExtractor = function (req) {
   let token = null;
   if (req && req.cookies) {
     token = req.cookies['jwt'];
   }
   //TODO : this is temporary token for testing without cookie
-  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjkxYjJkYmRkZjJlOGFhNGY4MjljMSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzEwODI0MjM3fQ.7wfQWBQSZZAkrzMHLudw5HbqHJC1uEOPXxigXJSJ0RE"
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjkxYjJkYmRkZjJlOGFhNGY4MjljMSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzEwODM2NTkxfQ.P77iRCJv-fSS4v5xcJyi7fZaE7MGTD51sHUZyWyb9SY"
   return token;
 };
